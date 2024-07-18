e8a57c76aab5139e465f1de9f5e83c810925b302
Gyuwon Hwang <113331205+kb081544@users.noreply.github.com>
2024-07-18 23:13:10 +0900

    fix filtering

--- filter.py
+++ filter.py
@@ -19,10 +19,6 @@ class preprocessing:
         self.overlap=overlap
 
     def chunk_data_hp(self):
-        #global x_result
-        """
-            입력 데이터를 청크(300)로 나누고 HeartPy 라이브러리를 사용하여 bandpass filter를 거쳐 심박수(HR) 특징을 추출합니다.
-        """
         data_y=[y[0] for y in self.data]
         data_x=[x[1:] for x in self.data]
         sum_removed=0
@@ -41,8 +37,6 @@ class preprocessing:
                                             filtertype='bandpass')
                 try: # chunk의 전처리 후 process 함수가 실행이 안되는 경우가 꽤나 많아, 에러 표시로 인한 코드 실행 중지를 방지하기 위해 try except 문으로 구현
                     wd, m = hp.process(filtered, sample_rate=25)
-
-                    """PPG 신호 피크가 존재하는 데이터에 대해서 빨갛게 표시된 피크들을 제거하고, 심박수 특징 추출"""
                     if (len(wd['peaklist']) != 0):
                         sum += (len(wd['peaklist']) - len(wd['removed_beats'])) #초록색 피크들의 총 합 계산(평균을 내기 위해)
                         sum_removed += len(wd['removed_beats']) #빨강색 피크
@@ -73,16 +67,18 @@ class preprocessing:
         print("cnt: ", cnt)
         print('exc: ', exc)
         print("y result: ", len(y_result))
-
+        avg = sum / cnt
         new_temp = 0
         new_cnt = 0
-        """ 트레인 데이터의 경우 bpm 평균을 구함 """
         if(self.tot=="train"):
             for j in range(cnt):
-                x_new_result.append(x_result[j])
-                y_new_result.append(y_result[j])
-                new_temp += m['bpm']
-                new_cnt += 1
+                if pk_np[j] > avg:
+                    x_new_result.append(x_result[j])
+                    y_new_result.append(y_result[j])
+                    new_temp += m['bpm']
+                    new_cnt += 1
+                else:
+                    continue
             new_avg = new_temp / new_cnt
             print("heartpy 평균: ", new_avg)
             print("cnt: ", cnt)
@@ -93,12 +89,16 @@ class preprocessing:
             return x_new_result, y_new_result
         elif(self.tot=="test"):
             return x_result, y_result
+
+    # 40,000 이라는 수를 조정하기 위한 함수를 하나 더 만들어보자.
+    # 기준을 확정해야 함.
+
     def dividing_and_extracting(self):
         x,y=self.chunk_data_hp()
         peak_shapes = []
         fake_index = []
-        # 각 청크의 최대값이 14000 이상인 인덱스를 데이터에서 제거 -> 가짜 피크
-        index = np.where(np.max(x, axis=1) >= 14000)[0]  # >= 14000, 알아서 자르는 수 조정
+        cutoff_n=40000
+        index = np.where(np.max(x, axis=1) >= cutoff_n)[0]
         new_data = np.delete(x, index, axis=0)
         new_data_y=np.delete(y, index, axis=0)
 
@@ -107,15 +107,15 @@ class preprocessing:
         for i in range(len(new_data)):
             temp = new_data[i, :]
             temp_y=new_data_y[i]
-            wd, m = hp.process(temp, sample_rate=25) # HeartPy로 신호 처리
+            wd, m = hp.process(temp, sample_rate=25)
 
-            peaks = wd['peaklist']  # 피크 리스트
-            fake_peaks = wd['removed_beats']  # 제거된 피크 리스트
+            peaks = wd['peaklist']
+            fake_peaks = wd['removed_beats']
             fake_index.extend(fake_peaks)
-            real_peaks = [item for item in peaks if item not in fake_peaks] # 실제 피크만 추출
+            real_peaks = [item for item in peaks if item not in fake_peaks]
             for index in real_peaks:
-                if not ((index - 13 < 0) or (index + 14 >= new_data.shape[1])): # 실제 피크 주변 27개의 신호를 포함할 수 있는지 확인
-                    peak_shape = temp[index - 13:index + 14] # 피크 주변 27 포인트 추출 -> 싱글펄스 (1.1초)
+                if not ((index - 13 < 0) or (index + 14 >= new_data.shape[1])):
+                    peak_shape = temp[index - 13:index + 14]
                     peak_shape = np.concatenate((np.array([temp_y]), peak_shape))
                     peak_shapes.append(peak_shape)
 
@@ -133,16 +133,15 @@ class preprocessing:
             data1 = data1[:, 1:]
 
             n_components = 2
-            # 긍정적인 클래스에 대한 GMM 모델 학습
             gmm_p = GaussianMixture(n_components=n_components, covariance_type='full')
             gmm_p.fit(data0)
 
-            # 긍정적인 클래스의 이상치 및 정상 데이터 분리
+            # 긍정 gmm model
             labels = gmm_p.predict(data0)
             outliers = data0[labels == 1]
             normals = data0[labels == 0]
 
-            # 부정적인 클래스에 대한 GMM 모델 학습
+            # 부정 gmm model
             gmm_n = GaussianMixture(n_components=n_components, covariance_type='full')
             gmm_n.fit(data1)
             labels = gmm_n.predict(data1)
@@ -152,10 +151,6 @@ class preprocessing:
             global lab1
             global lab0
 
-            # 각 클래스의 이상치와 정상 데이터의 평균을 비교하여 라벨 설정
-            # 부정데이터의 경우 평균이 더 큰 lable이 대표값으로 선정되고,
-            # 긍정데이터의 경우 평균이 더 작은 lable이 대표값으로 선정됨
-            # 그에따라 lab0, lab1이 정해짐
             if np.mean(normals_n) > np.mean(outliers_n):
                 spp1 = normals_n
                 lab1 = 0
@@ -171,9 +166,8 @@ class preprocessing:
 
             global m
             global n
-            m = np.max(spp1) #전체 데이터에서의 최대값
-            n = np.min(spp1) #전체 데이터에서의 최소값
-            # normalilzation 하는 부분
+            m = np.max(spp1)
+            n = np.min(spp1)
             normalized_train = []
             for value in spp0:
                 normalized_num = (value - n) / (m - n)
@@ -209,18 +203,15 @@ class preprocessing:
             d = d[:, 1:]
             tst = []
 
-            # GMM 모델을 사용하여 테스트 데이터의 라벨 예측 (긍정, 부정 모두)
             lb1 = gmm_n.predict(d)
             lb2 = gmm_p.predict(d)
 
-            #라벨이 lab1, lab2 중 하나라도 맞는 것이 없으면 이상치로 판단하여 제거
-            #즉, 테스트데이터가 gmm 모델에 적용되어 긍정 모델과 부정 모델 중 하나라도 맞지 않으면 pass됨
             for i in range(len(lb1)):
                 if lb1[i] != lab1 and lb2[i] != lab0:
                     pass
                 else:
                     tst.append(d[i])
-            # 정규화 과정
+
             normalized = []
             for value in tst:
                 normalized_num = (value - n) / (m - n)
